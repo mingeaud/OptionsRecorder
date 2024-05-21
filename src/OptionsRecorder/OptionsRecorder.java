f2d2fc3a8abdc43afee45baef2531a089c12633d
mingeaud <mingeaud@oregonstate.edu>
2024-05-20 19:21:49 -0700

    Created front panel and Security object

--- src/OptionsRecorder/OptionsRecorder.java
+++ src/OptionsRecorder/OptionsRecorder.java
@@ -1,11 +1,38 @@
 package OptionsRecorder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+//import OptionsRecorder.FrontPanel;
+
 public class OptionsRecorder {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-
+		
+		try {
+            // Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    } 
+    catch (UnsupportedLookAndFeelException e) {
+       // handle exception
+    }
+    catch (ClassNotFoundException e) {
+       // handle exception
+    }
+    catch (InstantiationException e) {
+       // handle exception
+    }
+    catch (IllegalAccessException e) {
+       // handle exception
+    }
+		
+	FrontPanel mainLoop = new FrontPanel();
+		
 	}
 
 }
